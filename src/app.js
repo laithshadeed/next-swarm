59d96bcc251b120d25e039ac7f15d2c92f57c0a0
Marcel Toele <EleotleCram@gmail.com>
2015-11-05 11:25:02 +0100

    app.js: Now uses consistent semi style

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-var finalhandler = require('finalhandler')
-var http = require('http')
-var serveIndex = require('serve-index')
-var serveStatic = require('serve-static')
-var serveCachedStatic = require('connect-static')
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveIndex = require('serve-index');
+var serveStatic = require('serve-static');
+var serveCachedStatic = require('connect-static');
 
 // Serve directory indexes for public/ftp folder (with icons)
-var index = serveIndex('public', {'icons': true})
+var index = serveIndex('public', {'icons': true});
 
 // Serve up public/ftp folder files
-//var serve = serveStatic('public')
+//var serve = serveStatic('public');
 
 serveCachedStatic({}, function (err, serve) {
 	if (err) {
@@ -17,15 +17,15 @@ serveCachedStatic({}, function (err, serve) {
 
 	// Create server
 	var server = http.createServer(function onRequest(req, res) {
-		var done = finalhandler(req, res)
+		var done = finalhandler(req, res);
 		serve(req, res, function onNext(err) {
 			if (err) {
-				return done(err)
+				return done(err);
 			}
-			index(req, res, done)
+			index(req, res, done);
 		})
 	})
 
 	// Listen
-	server.listen(3000)
+	server.listen(3000);
 });

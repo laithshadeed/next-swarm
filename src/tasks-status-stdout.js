75ec726a2cf736e5578f11b2bd9da39ff2dd66a9
Marcel Toele <EleotleCram@gmail.com>
2015-11-17 02:23:09 +0100

    tasks-status-stdout.js: Added better console logging (more overview of status of many tasks)

--- src/tasks-status-stdout.js
+++ src/tasks-status-stdout.js
@@ -1,7 +1,23 @@
+var sprintf=require("sprintf-js").sprintf;
+var _ = require("underscore-node");
 var bus = require("hermes-bus");
 
 function reportTasks(tasks) {
-	console.log(tasks.toString());
+	console.log('\033[2J'); // clear console (ANSI terminal/VT100)
+	console.log("Tasks:");
+	console.log("           Test Name              |    Status    |   Worker Id    | Completed? |Retry| Report");
+	console.log(tasks.map(function(task) {
+		var taskCopy = {
+			name:		task.name.substr(-30),
+			status:		task.status,
+			completed:	task.completed ? "Completed" : "",
+			workerId:	task.workerId,
+			numRecoveryRunsLeft: 3-(task.numRecoveryRunsLeft || 3),
+			report:		JSON.stringify(_.pick(task.report, "fail", "error", "total")),
+		};
+		taskCopy.name = task.name.substr(-30);
+		return sprintf("   %(name)30s | %(status)12s | %(workerId)14s | %(completed)10s | %(numRecoveryRunsLeft)3s | %(report)s", taskCopy);
+	}).join("\n"));
 	console.log("");
 }
 

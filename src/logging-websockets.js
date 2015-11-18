bdae258216ffc468b2c06cef4a26a5c0fe9f57dc
Marcel Toele <EleotleCram@gmail.com>
2015-11-18 11:40:11 +0100

    logging-websockets.js: Now adds a link to the web client task reporting

--- src/logging-websockets.js
+++ src/logging-websockets.js
@@ -1,3 +1,4 @@
+var sprintf=require("sprintf-js").sprintf;
 var _ = require('underscore-node');
 var bus = require("hermes-bus");
 
@@ -20,6 +21,34 @@ bus.on("logReset", function(loggerId){
 	if(_.contains(supportedLogAreaNames, loggerId)) {
 		logAreas[loggerId] = [];
 	}
+});
+
+// Adds a link to the web client reporting page
+bus.on("logReset", function(loggerId){
+	if(loggerId === "dashboard" && localConnectServerUri) {
+		bus.triggerLogMessage("dashboard", "\033[H" + sprintf("%"+process.stdout.columns+"s", "Interactive log also available at: "+localConnectServerUri+"/report-task-status/"));
+	}
+});
+
+// @TODO remove duplicate code
+var localIpAddress = "127.0.0.1";
+
+bus.on("applicationStarted", function() {
+	var firstNonLocalNetworkConfig = _.values(os.networkInterfaces()).flatten().find((e) => e.address !== "127.0.0.1" && e.address !== "::1" );
+
+	if(firstNonLocalNetworkConfig) {
+		localIpAddress = firstNonLocalNetworkConfig.address;
+	} else {
+		console_log("Error: Unable to determine public ip address of this host!");
+		console_log( "Exiting...");
+		bus.triggerRequestStopApplication({value: 1});
+	}
+});
+
+var os = require('os');
+var localConnectServerUri;
+bus.on("connectServerStarted", function(connectServer) {
+	localConnectServerUri = "http://"+localIpAddress+":"+connectServer.address().port;
 });
 
 var WebSocketServer = require('ws').Server;

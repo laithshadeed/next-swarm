2912c31473bc7f14714e01f296f0aef23e7ad94d
Marcel Toele <EleotleCram@gmail.com>
2017-07-03 14:42:33 +0200

    src/logging-vt100.js: Added --vt100-log-refresh-interval option

--- src/logging-vt100.js
+++ src/logging-vt100.js
@@ -8,14 +8,37 @@ var logAreas = {
 };
 var supportedLogAreaNames = Object.keys(logAreas);
 
+var logToVT100 = function() {
+	console.log("\033c");
+	console.log(logAreas.dashboard.join("\n"));
+	console.log(sprintf("\n%'="+process.stdout.columns+"s\n", ""));
+	console.log(logAreas.console.slice(-10).join("\n"));
+};
+var DEFAULT_LOG_INTERVAL_SEC = 0.04;
+var throttledLogToVT100 = _.throttle(logToVT100, DEFAULT_LOG_INTERVAL_SEC * 1000);
+bus.on("registerCommandlineArguments", function (parser) {
+	parser.addArgument(
+		['--vt100-log-refresh-interval'],
+		{
+			dest: 'logRefreshInterval',
+			metavar: 'SEC',
+			type: 'float',
+			help: 'The interval with which the log output on the command line is refreshed.'
+		}
+	);
+});
+bus.on("commandlineArgumentsParsed", function (args) {
+	throttledLogToVT100 = _.throttle(
+		logToVT100,
+		(args.logRefreshInterval || DEFAULT_LOG_INTERVAL_SEC) * 1000
+	);
+});
+
 bus.on("logMessage", function(loggerId, message){
 	if(_.contains(supportedLogAreaNames, loggerId)) {
 		logAreas[loggerId].push.apply(logAreas[loggerId], message.split(/\n/));
 
-		console.log("\033c");
-		console.log(logAreas.dashboard.join("\n"));
-		console.log(sprintf("\n%'="+process.stdout.columns+"s\n", ""));
-		console.log(logAreas.console.slice(-10).join("\n"));
+		throttledLogToVT100();
 	}
 });
 
